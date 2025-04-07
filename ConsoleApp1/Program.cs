8776d3ce3be2cb96d29d30bb0e7798d7709337e2
OlenaKut <105447315+OlenaKut@users.noreply.github.com>
2025-04-07 12:30:37 +0200

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace MainMenu;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Main Menu!");
+
+
+        bool isAlive = true;
+
+        do
+        {
+            Console.WriteLine("Please press 0 to exit:");
+            string input = Console.ReadLine().ToUpper() ?? string.Empty;
+
+            switch (input)
+            {
+                case "0":
+                    isAlive = false;
+                    break;
+                default:
+                    Console.WriteLine("Please select a valid choice!");
+                    break;
+            }
+
+        } while (isAlive);
+
+    }
+}
+
